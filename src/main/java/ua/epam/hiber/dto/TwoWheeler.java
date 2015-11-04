b051951c1fc8aefdcb8895996ffb5f3c73398af3
Dmytro_Adonin <dmytro_adonin@epam.com>
2015-11-04 16:49:32 +0200

    SingleTable, Discriminator

--- src/main/java/ua/epam/hiber/dto/TwoWheeler.java
+++ src/main/java/ua/epam/hiber/dto/TwoWheeler.java
@@ -1,11 +1,13 @@
 package ua.epam.hiber.dto;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 /**
  * Created by Dmytro_Adonin on 11/4/2015.
  */
 @Entity
+@DiscriminatorValue(value = "Bike")
 public class TwoWheeler extends Vehicle {
 
     private String steeringHandle;
--- src/main/java/ua/epam/hiber/dto/Vehicle.java
+++ src/main/java/ua/epam/hiber/dto/Vehicle.java
@@ -7,6 +7,8 @@ import javax.persistence.*;
  */
 @Entity
 @Table(name = "VEHICLE")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "VEHICLE_TYPE", discriminatorType = DiscriminatorType.STRING)
 public class Vehicle {
 
     @Id
