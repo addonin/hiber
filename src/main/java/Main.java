ad59a17019b2d6c8b29b5b07fd7202b0d056d15c
Dmytro_Adonin <dmytro_adonin@epam.com>
2015-11-06 11:17:38 +0200

    Criteria API

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+import ua.epam.hiber.dto.UserDetails;
 
 import java.util.List;
 
@@ -23,21 +26,26 @@ public class Main {
         Query unusedQuery = session.createQuery("from UserDetails where id > ?");
         unusedQuery.setInteger(0, 1);
 
-        Query query = session.createQuery("select userName from UserDetails where id > :id and userName = :userName");
+        /*Query query = session.createQuery("select userName from UserDetails where id > :id and userName = :userName");
         query.setInteger("id", 1);
         query.setString("userName", "User3");
-        List<String> list = query.list();
+        List<String> list = query.list();*/
 
-        query = session.getNamedQuery("userName.byId");
+        /*query = session.getNamedQuery("userName.byId");
         query.setInteger("id", 1);
-        list = query.list();
+        list = query.list();*/
+
+        Criteria criteria = session.createCriteria(UserDetails.class);
+        //fluent interface equals to AND, so the next example for OR
+        criteria.add(Restrictions.or(Restrictions.eq("id", 1), Restrictions.gt("id", 3)));
+        List<UserDetails> list = criteria.list();
 
         session.getTransaction().commit();
         session.close();
 
         //System.out.println("List length: " + list.size());
-        for (String name : list) {
-            System.out.println("Name : " + name);
+        for (UserDetails user : list) {
+            System.out.println("Name : " + user.getUserName());
         }
     }
 }
